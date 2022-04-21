f336dfedb995bfd9aec27e69405b10fc51879132
Fox_Coder508 <mahmood.ademoye16@gmail.com>
2022-04-21 18:37:02 +0100

    scroll project - working on fixing scrolling

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,10 +4,70 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.querySelector(".date");
+date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
+navToggle.addEventListener('click', () => {
+    //linksContainer.classList.toggle('show-links');
+
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`;
+    }
+    else{
+        linksContainer.style.height = 0;
+    }
+})
+
+
+const navBar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 // ********** fixed navbar ************
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navBar.getBoundingClientRect().height;
+
+    if(scrollHeight > navHeight){
+        navBar.classList.add('fixed-nav');
+    }
+    else{
+        navBar.classList.remove('fixed-nav');
+    }
+
+    if(scrollHeight > 700){
+        topLink.classList.add('show-link');
+    }
+    else{
+        topLink.classList.remove('show-link');
+    }
+})
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach( (link) => {
+    link.addEventListener('click', (e) => {
+        //prevent default event
+        e.preventDefault;
+
+        //navigate to the specific element location on the page
+        const id  = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        let position = element.offsetTop;
+
+        window.scrollTo(
+            {
+                left: 0,
+                top: position
+            }
+        )
+        linksContainer.style.height = 0; 
+    })
+})
